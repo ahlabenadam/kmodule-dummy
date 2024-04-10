9644606bc87474b1395800b6422f3ef9dfd6ca65
Aleksey Senin <alekseys@nvidia.com>
2024-04-10 14:56:36 -0500

    Redesigned

--- dummydevice.c
+++ dummydevice.c
@@ -1,23 +1,27 @@
 /**
  * @file   dummydevice.c
- * @author Alan-Lee Perkins
- * @date   14 July 2019
- * @version 0.1
- * @brief   a dummy device device - kernel module - usable as an example for building a kernel module
- * @see https://github.com/itandarts/kmodule-dummy for more
+ * @author Aleksey Senin
+ * @date   Apr 2024
+ * @version 1.0
+ * @brief   a dummy device device
+ * @see  https://github.com/ahlabenadam/kmodule-dummy
  */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/device.h>
+#include <linux/kdev_t.h>
+#include <linux/cdev.h>
 
 #define DEVICE_NAME "dummydevice"
 
-MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
-MODULE_AUTHOR("Alan-Lee Perkins");    ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("DummyDevice Model for the linux kernel");  ///< The description -- see modinfo
-MODULE_VERSION("0.1");            ///< A version number to inform users
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Aleksey Senin");
+MODULE_DESCRIPTION("DummyDevice character device Module");
+MODULE_VERSION("1.0");            ///< A version number to inform users
 
 
 static int dev_open(struct inode*, struct file*);
@@ -26,28 +30,59 @@ static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
 static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
 
 static struct file_operations fops = {
-   .open = dev_open,
-   .read = dev_read,
-   .write = dev_write,
-   .release = dev_release,
+    .owner = THIS_MODULE,
+    .open = dev_open,
+    .read = dev_read,
+    .write = dev_write,
+    .release = dev_release,
 };
 
-static int major;
+struct dmodule {
+    struct class *cl;
+    struct cdev c_dev;
+    dev_t major;
+};
+
+static struct dmodule dmod;
 
 static int __init dummydevice_init(void) {
-    major = register_chrdev(0, DEVICE_NAME, &fops);
+    int ret;
 
-    if (major < 0) {
-        printk(KERN_ALERT "dummydevice load failed\n");
-        return major;
+    if ((ret = alloc_chrdev_region(&dmod.major, 0, 1, "dummyregion")) < 0 )
+        return ret;
+
+    if ((dmod.cl  = class_create("dummychr")) == NULL) {
+        printk(KERN_ALERT "Class create failed\n");
+        goto l_chrdev;
     }
 
-    printk(KERN_INFO "dummydevice module has been loaded: %d\n", major);
+    if (device_create(dmod.cl, NULL, dmod.major, NULL, "dummydev") == NULL) {
+        printk(KERN_ALERT "device create failed\n");
+        goto l_class;
+    }
+
+    cdev_init(&dmod.c_dev, &fops);
+
+    if (cdev_add(&dmod.c_dev, dmod.major, 1) == -1 ) {
+        printk(KERN_ALERT "adding device failed\n");
+        goto l_cdev_add;
+    }
     return 0;
+
+l_cdev_add:
+    device_destroy(dmod.cl, dmod.major);
+l_class:
+    class_destroy(dmod.cl);
+l_chrdev:
+    unregister_chrdev_region(dmod.major, 1);
+    return -ENOMEM;
 }
 
 static void __exit dummydevice_exit(void) {
-    unregister_chrdev(major, DEVICE_NAME);
+    cdev_del(&dmod.c_dev);
+    device_destroy(dmod.cl, dmod.major);
+    class_destroy(dmod.cl);
+    unregister_chrdev_region(dmod.major, 1);
     printk(KERN_INFO "dummydevice module has been unloaded\n");
 }
 
@@ -70,7 +105,7 @@ static int dev_release(struct inode *inodep, struct file *filep) {
 
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset) {
     int errors = 0;
-    char *message = "dummydevice read response!";
+    char *message = "dummydevice read response!\n";
     int message_len = strlen(message);
 
     errors = copy_to_user(buffer, message, message_len);
